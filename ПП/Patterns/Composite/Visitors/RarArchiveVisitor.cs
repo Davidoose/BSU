2af43b8a3a3b6cddb38e957856a0674b321d71f2
Давид <108413873+Davidoose@users.noreply.github.com>
2022-11-01 21:48:54 +0300

    Command anf Composite

---
+++ ПП/Patterns/Composite/Visitors/RarArchiveVisitor.cs
@@ -0,0 +1,20 @@
+﻿using Composite.Components;
+using Composite.Interfaces;
+
+namespace Composite.Visitors
+{
+    public class RarArchiveVisitor : IArchiveVisitor
+    {
+        public void Archive(FileComponent fileComponent)
+        {
+            Console.WriteLine("Rar..");
+            Console.WriteLine(fileComponent.Name);
+        }
+
+        public void Archive(Folder folder)
+        {
+            Console.WriteLine("Rar..");
+            Console.WriteLine(folder.Name);
+        }
+    }
+}
