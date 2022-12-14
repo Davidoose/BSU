637dc9b3ebec876ebaf9c1a99ece8bdae80d2ae0
Давид <108413873+Davidoose@users.noreply.github.com>
2022-12-15 01:37:26 +0300

    State

---
+++ ПП/Patterns/State/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace State
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var dbConnection = new DbConnection();
+
+            string str = "SELECT * FROM ...";
+
+            dbConnection.Open();
+            dbConnection.Process(str);
+            dbConnection.Close();
+        }
+    }
+}
