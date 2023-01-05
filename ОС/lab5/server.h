bd8b82d834c981c75ac9c27c763a181e61b1bd65
Давид <108413873+Davidoose@users.noreply.github.com>
2023-01-05 21:29:02 +0300

    deleted the bad version and rewrote

---
+++ ОС/lab5/server.h
@@ -0,0 +1,21 @@
+    #ifndef LAB5_SERVER_H
+#define LAB5_SERVER_H
+
+
+#include <ostream>
+class server {
+public:
+    int num;
+    char name[10];
+    double hours;
+    void print(std::ostream &out){
+        out << "ID: " << num << "\tName: " << name << "\tHours: " << hours << std::endl;
+    }
+};
+
+int compare(const void* p1, const void* p2){
+    return ((server*)p1)->num - ((server*)p2)->num;
+}
+
+
+#endif //LAB5_SERVER_H
